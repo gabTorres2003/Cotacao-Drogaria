d0002853619f58eea77a9f58c6bdb378eef28276
Gabriel Torres <gabrielstorres08@gmail.com>
2026-01-19 14:02:38 -0300

    Config CORS Global

---
+++ backend-api/cotacao/cotacao/src/main/java/com/drogaria/cotacao/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.drogaria.cotacao.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Aplica a todas as rotas
+                .allowedOrigins("*") // LIBERA GERAL Netlify, Localhost, etc
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Libera todos os métodos
+                .allowedHeaders("*"); // Libera todos os cabeçalhos
+    }
+}
