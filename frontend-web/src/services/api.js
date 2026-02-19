ade92649b88c286e678cf0f47f96408273335adf
Gabriel Torres <gabrielstorres08@gmail.com>
2026-02-19 20:30:27 -0300

    update base url to local server

--- frontend-web/src/services/api.js
+++ frontend-web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://tray-element-earthquake-newman.trycloudflare.com',
+  baseURL: 'https://basics-arlington-morning-actions.trycloudflare.com', 
 });
 
 api.interceptors.request.use((config) => {
