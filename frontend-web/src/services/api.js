916318f7434b7bedf618294b76d28a286a679712
Gabriel Torres <gabrielstorres08@gmail.com>
2026-03-01 18:21:31 -0300

    fix: injeção do .env para testes

--- frontend-web/src/services/api.js
+++ frontend-web/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.drogariatorresfarma.com.br', 
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 api.interceptors.request.use((config) => {
