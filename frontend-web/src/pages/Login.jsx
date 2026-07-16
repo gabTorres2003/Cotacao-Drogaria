45d1652d0c24fdd1e0f10fbe3d9f476dc6f5f22d
Gabriel Torres <gabrielstorres08@gmail.com>
2026-07-16 14:18:30 -0300

    Update Login.jsx

--- frontend-web/src/pages/Login.jsx
+++ frontend-web/src/pages/Login.jsx
@@ -29,7 +29,7 @@ export default function Login() {
         localStorage.setItem('token', token)
       }
 
-      navigate('/dashboard')
+      navigate('/cotacoes')
     } catch (error) {
       setErro('E-mail ou senha inválidos.')
     } finally {
