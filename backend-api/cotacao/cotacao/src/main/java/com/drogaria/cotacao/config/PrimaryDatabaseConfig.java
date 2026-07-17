6d8a1b2e469f69f4bae079f126e96ad11383985d
Gabriel Torres <gabrielstorres08@gmail.com>
2026-07-17 12:12:55 -0300

    Update PrimaryDatabaseConfig.java

--- backend-api/cotacao/cotacao/src/main/java/com/drogaria/cotacao/config/PrimaryDatabaseConfig.java
+++ backend-api/cotacao/cotacao/src/main/java/com/drogaria/cotacao/config/PrimaryDatabaseConfig.java
@@ -1,7 +1,7 @@
 package com.drogaria.cotacao.config;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -11,10 +11,28 @@ import javax.sql.DataSource;
 @Configuration
 public class PrimaryDatabaseConfig {
 
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUser;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String dbDriver;
+
     @Primary
     @Bean(name = "dataSource")
-    @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource primaryDataSource() {
-        return DataSourceBuilder.create().build();
+        HikariDataSource dataSource = new HikariDataSource();
+        
+        dataSource.setJdbcUrl(dbUrl); 
+        dataSource.setUsername(dbUser);
+        dataSource.setPassword(dbPassword);
+        dataSource.setDriverClassName(dbDriver);
+        
+        return dataSource;
     }
 }
