ba2182ca66d86c2e7857cea31e27a5aff7ab73ea
Gabriel Torres <gabrielstorres08@gmail.com>
2026-07-17 12:06:24 -0300

    Create PrimaryDatabaseConfig.java

---
+++ backend-api/cotacao/cotacao/src/main/java/com/drogaria/cotacao/config/PrimaryDatabaseConfig.java
@@ -0,0 +1,20 @@
+package com.drogaria.cotacao.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PrimaryDatabaseConfig {
+
+    @Primary
+    @Bean(name = "dataSource")
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
