dc25ca90268cd601c83feda277f4a45bcca05bc4
Gabriel Torres <gabrielstorres08@gmail.com>
2026-08-15 19:36:21 -0300

    Update EncomendaRepository.java

--- backend-api/cotacao/cotacao/src/main/java/com/drogaria/cotacao/repository/EncomendaRepository.java
+++ backend-api/cotacao/cotacao/src/main/java/com/drogaria/cotacao/repository/EncomendaRepository.java
@@ -2,12 +2,10 @@ package com.drogaria.cotacao.repository;
 
 import com.drogaria.cotacao.model.Encomenda;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 import java.util.UUID;
 
 public interface EncomendaRepository extends JpaRepository<Encomenda, UUID> {
-    
-    @Query(value = "SELECT * FROM encomendas WHERE comprado = false OR comprado IS NULL", nativeQuery = true)
-    List<Encomenda> findByCompradoFalseOrCompradoIsNull();
+
+    List<Encomenda> findByCompradoFalseOrCompradoIsNull();   
 }
